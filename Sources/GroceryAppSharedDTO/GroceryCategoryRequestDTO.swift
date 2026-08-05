30b9a4d563b7c7e5da2503fab814102b596b94bf
sflorido <sflorido@gmail.com>
2026-08-05 13:46:18 -0300

    Add response and request for grocery category create route.

---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryCategoryRequestDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Silvia Florido on 05/08/26.
+//
+
+import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    public let title: String
+    public let color: String
+    
+    public init(title: String, color: String) {
+        self.title = title
+        self.color = color
+    }
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -0,0 +1,20 @@
+//
+//  GroceryCategoryResponseDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Silvia Florido on 05/08/26.
+//
+
+import Foundation
+
+public struct GroceryCategoryResponseDTO: Codable {
+    public let id: UUID
+    public let title: String
+    public let color: String
+    
+    public init(id: UUID, title: String, color: String) {
+        self.id = id
+        self.title = title
+        self.color = color
+    }
+}
