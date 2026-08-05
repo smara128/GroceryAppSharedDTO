5c1e01c509e00398f406de5161c01d1bcf7bf630
sflorido <sflorido@gmail.com>
2026-08-04 22:42:39 -0300

    Initial commit

---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterResponseDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Silvia Florido on 04/08/26.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
