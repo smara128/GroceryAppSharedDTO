822d44d1d0ef2ac53618ab73c1f3133eb592c534
sflorido <sflorido@gmail.com>
2026-08-25 13:18:55 -0300

    Adds items to Category response.

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -11,10 +11,12 @@ public struct GroceryCategoryResponseDTO: DTO, Sendable {
     public let id: UUID
     public let title: String
     public let color: String
+    public let items: [GroceryItemResponseDTO]
     
-    public init(id: UUID, title: String, color: String) {
+    public init(id: UUID, title: String, color: String, items: [GroceryItemResponseDTO] = []) {
         self.id = id
         self.title = title
         self.color = color
+        self.items = items
     }
 }
